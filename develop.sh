b596b10752136cd805b3b3f0507ac2a2e2d414fc
Benjamin Thomas Graham <btgraham@gmail.com>
2018-10-23 13:28:10 -0700

    headers

--- develop.sh
+++ develop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 rm -rf build/ dist/ sparseconvnet.egg-info sparseconvnet_SCN*.so
 python setup.py develop
 python examples/hello-world.py

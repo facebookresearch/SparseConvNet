dcd1428dcc769d175734260764eeadf160593335
Benjamin Graham <benjamingraham@fb.com>
2018-12-18 09:30:16 +0000

    BSD license

--- sparseconvnet/SCN/CPU/ActivePooling.cpp
+++ sparseconvnet/SCN/CPU/ActivePooling.cpp
@@ -1,7 +1,7 @@
 // Copyright 2016-present, Facebook, Inc.
 // All rights reserved.
 //
-// This source code is licensed under the license found in the
+// This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
 // Assume output_features and d_input_features have been zero-ed

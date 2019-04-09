32f8cbcf0869d6f631eb86ec003f01bf20030233
Ben Graham <btgraham@gmail.com>
2019-04-09 10:45:44 +0100

    header

--- examples/nyu2/data/prepare_data.py
+++ examples/nyu2/data/prepare_data.py
@@ -1,3 +1,9 @@
+# Copyright 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 import torch
 import glob, math, os

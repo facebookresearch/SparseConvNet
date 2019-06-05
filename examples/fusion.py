2d4905e7801f45ca1868e0b9225e4a23553ad71e
Yueh Cheng Liu <jimmydiablo@gmail.com>
2019-06-05 05:55:52 +0000

    Update fusion example

--- examples/fusion.py
+++ examples/fusion.py
@@ -1,13 +1,15 @@
 import sparseconvnet as scn
 import torch
+from torch.autograd import Variable
 
 input_layer = scn.Sequential()\
             .add(scn.InputLayer(2, 200))\
 
 fusion_layer = scn.GlobalFusion(2, 1.8)
 
+
 global_locs = [
-    [0, 0], [0, 1], [1, 0], [1, 1], [10, 10]
+    [0, 0], [0, 1], [1, 0], [1, 1]
 ]
 global_feats = [
     [.1, .2, .3],
@@ -39,14 +41,20 @@ local_feats = [
     [6, 6],
 ]
 local_locs = torch.Tensor(local_locs)
-local_feats = torch.Tensor(local_feats)
+local_feats = torch.Tensor(local_feats).cuda()
+local_feats = Variable(local_feats, requires_grad=True)
 global_locs = torch.Tensor(global_locs)
-global_feats = torch.Tensor(global_feats)
+global_feats = torch.Tensor(global_feats).cuda()
+global_feats = Variable(global_feats, requires_grad=True)
 x = input_layer([local_locs, local_feats])
 y = input_layer([global_locs, global_feats])
 print(x)
+print(fusion_layer)
 
-base = torch.Tensor([[0, 0, 0]])
-base.requires_grad_(False)
-fuse = fusion_layer(x, y, base, base)
-print(fuse)
+local_base = torch.Tensor([[1, 1]])
+global_base = torch.Tensor([[1, 1]])
+# base.requires_grad_(False)
+fuse = fusion_layer(x, y, local_base, global_base)
+# print(fuse)
+print(fuse.features)
+print(fuse.features.backward(fuse.features))
